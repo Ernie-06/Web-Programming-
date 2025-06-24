52934f858e2892b6d1e2dcdddc3d913de42d3c1f
Ernaldo Becerra <ernaldito12@gmail.com>
2025-06-24 14:24:54 -0700

    agregue js a los inputs de mascotas

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,27 @@
-function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE TU MASCOTA")
+function iniciarJuego() {
+
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
 
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador) 
+
+function seleccionarMascotaJugador() {
+
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+
+    if (inputHipodoge.checked) {
+        alert("Elegiste a Hipodoge")
+    } else if (inputCapipepo.checked) {
+        alert("Elegiste a Capipepo")
+    } else if (inputRatigueya.checked) {
+        alert("Elegiste a Ratigueya")
+    } else {
+        alert("Selecciona una mascota")
+    }
+}
+
+
+window.addEventListener("load", iniciarJuego)
