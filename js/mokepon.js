47ef92a9b8b0e91a96ac962104add195db654f3d
Ernaldo Becerra <ernaldito12@gmail.com>
2025-06-25 20:32:21 -0700

    cambios a js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,27 +1,26 @@
 function iniciarJuego() {
-
-    let botonMascotaJugador = document.getElementById("boton-mascota")
-    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+  let botonMascotaJugador = document.getElementById("boton-mascota");
+  botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 }
-
-
 
 function seleccionarMascotaJugador() {
+  // Mi primer variable manda a llamar el elemento de mi HTML hipodoge.
+  let inputHipodoge = document.getElementById("hipodoge");
+  // Esta variable manda a traer mi segun elemento de capipepo
+  let inputCapipepo = document.getElementById("capipepo");
 
-    let inputHipodoge = document.getElementById("hipodoge")
-    let inputCapipepo = document.getElementById("capipepo")
-    let inputRatigueya = document.getElementById("ratigueya")
+  //
+  let inputRatigueya = document.getElementById("ratigueya");
 
-    if (inputHipodoge.checked) {
-        alert("Elegiste a Hipodoge")
-    } else if (inputCapipepo.checked) {
-        alert("Elegiste a Capipepo")
-    } else if (inputRatigueya.checked) {
-        alert("Elegiste a Ratigueya")
-    } else {
-        alert("Selecciona una mascota")
-    }
+  if (inputHipodoge.checked) {
+    alert("Elegiste a Hipodoge");
+  } else if (inputCapipepo.checked) {
+    alert("Elegiste a Capipepo");
+  } else if (inputRatigueya.checked) {
+    alert("Elegiste a Ratigueya");
+  } else {
+    alert("Selecciona una mascota");
+  }
 }
 
-
-window.addEventListener("load", iniciarJuego)
+window.addEventListener("load", iniciarJuego);
