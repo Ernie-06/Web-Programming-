41af60eafee999696fb7772b2672ab0b2bab5202
Ernaldo Becerra <ernaldito12@gmail.com>
2025-08-23 20:37:23 -0700

    agregando express con node

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola Node");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola Con Express");
+});
+
+app.listen(4004, () => {
+  console.log("Servidor Funcionando");
+});
