f6e83166fe7ea709d579c12411eda60d9558f3c4
Ernaldo Becerra <ernaldito12@gmail.com>
2025-09-03 13:51:31 -0700

    comunicacion con json

--- js/mokepon.js
+++ js/mokepon.js
@@ -20,6 +20,7 @@ const contenedorAtaques = document.getElementById("contenedorAtaques");
 const sectionVerMapa = document.getElementById("ver-mapa");
 const mapa = document.getElementById("mapa");
 
+let jugadorId = null;
 let mokepones = [];
 let ataqueJugador = [];
 let ataqueEnemigo = [];
@@ -202,6 +203,7 @@ function unirseAlJuego() {
     if (res.ok) {
       res.text().then(function (respuesta) {
         console.log(respuesta);
+        jugadorId = respuesta;
       });
     }
   });
@@ -223,9 +225,22 @@ function seleccionarMascotaJugador() {
     alert("Selecciona una mascota");
   }
 
+  seleccionarMokepon(mascotaJugador);
+
   extraerAtaques(mascotaJugador);
   sectionVerMapa.style.display = "flex";
   iniciarMapa();
+}
+function seleccionarMokepon(mascotaJugador) {
+  fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      mokepon: mascotaJugador,
+    }),
+  });
 }
 
 function extraerAtaques(mascotaJugador) {
